6b77bfc4acaca9fbb6e758431edfff8da6664931
Abiud kiplagat beren <berenabiud4@gmail.com>
2024-12-16 23:44:25 +0300

    modified models.py

--- models.py
+++ models.py
@@ -4,15 +4,6 @@ from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
-
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
 
 class Game(Base):
     __tablename__ = 'games'
